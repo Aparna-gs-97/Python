f476524340710c9c0fa5c757b3692310f518451c
Aparna-gs-97 <62695161+Aparna-gs-97@users.noreply.github.com>
2020-03-26 20:44:17 +0530

    Add files via upload

---
+++ python_assignment1.py
@@ -0,0 +1,43 @@
+Python 3.8.1 (tags/v3.8.1:1b293b6, Dec 18 2019, 22:39:24) [MSC v.1916 32 bit (Intel)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+= RESTART: C:\Users\hp\AppData\Local\Programs\Python\Python38-32\assignment_1.py
+Enter any number upto which squares should print: 7
+
+ 0
+
+ 1
+
+ 4
+
+ 9
+
+ 16
+
+ 25
+
+ 36
+Enter the number of scores  : 3
+Enter the score : 12
+Enter the score : 34
+Enter the score : 56
+
+The entered scores are : 
+[12, 34, 56]
+
+The runnerup's score is :  34
+Enter any string to swap cases : AkankshA
+
+The swapped case of the given string is : aKANKSHa
+Enter the number of elements in the list: 6
+Enter the element: 3
+Enter the element: 6
+Enter the element: 7
+Enter the element: 9
+Enter the element: 1
+Enter the element: 0
+
+The provided list is :  [3, 6, 7, 9, 1, 0]
+
+The odd indexed elements of the given list is : [6, 9, 0]
+>>> 
